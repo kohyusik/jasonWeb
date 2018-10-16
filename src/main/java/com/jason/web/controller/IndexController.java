ab6e5d71adfb022be756eba45cbc454f9429665d
Yusik Koh <yusik.koh@mytrum.com>
2018-10-16 17:35:46 +0900

    thymeleaf init & test code

--- src/main/java/com/jason/web/controller/IndexController.java
+++ src/main/java/com/jason/web/controller/IndexController.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
 /**
  * @author : kohyusik
  * @version : 1.0
@@ -16,9 +19,18 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class IndexController {
     
     @GetMapping(path = { "/index", "/" })
-    @ResponseBody
     public String index() {
     
-        return "say hello";
+        return "index";
+    }
+    
+    @GetMapping(path = { "/ping" })
+    @ResponseBody
+    public String ping() {
+    
+        ZonedDateTime now = ZonedDateTime.now();
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("MM/dd/yyyy HH:mm:ss.SSS '('OOOO')'");
+        
+        return now.format(dateTimeFormatter);
     }
 }
--- src/test/java/com/jason/web/controller/IndexControllerTest.java
+++ src/test/java/com/jason/web/controller/IndexControllerTest.java
@@ -1,5 +1,15 @@
 package com.jason.web.controller;
 
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
+import static junit.framework.TestCase.assertEquals;
+
 /**
  * @author : kohyusik
  * @version : 1.0
@@ -7,5 +17,22 @@ package com.jason.web.controller;
  * @description :
  */
 public class IndexControllerTest {
+    
+    Logger log = LoggerFactory.getLogger(getClass());
+    
+    @Test
+    public void pingTest() {
+    
+        ZonedDateTime now = ZonedDateTime.of(
+                2018, 10, 16, 15, 7, 6, 169 * 1000000,
+                ZoneId.of("Asia/Seoul"));
+        
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("MM/dd/yyyy HH:mm:ss.SSS '('OOOO')'");
+    
+        String nowString = now.format(dateTimeFormatter);
+        log.debug(nowString);
+        
+        assertEquals("10/16/2018 15:07:06.169 (GMT+09:00)", nowString);
+    }
     
 }
