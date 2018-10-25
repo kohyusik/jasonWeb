20a8fe2406ef98c3e1d0751bca0aec50e245635f
kohyusik <newtype26@naver.com>
2018-10-25 22:55:12 +0900

    member test

--- src/test/java/com/jason/web/member/MemberTest.java
+++ src/test/java/com/jason/web/member/MemberTest.java
@@ -4,6 +4,9 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author : kohyusik
  * @version : 1.0
@@ -17,5 +20,38 @@ public class MemberTest {
 
     @Test
     public void saveTest() {
+
+        int a = 10;
+        int b = a;
+
+        a = 4;
+
+        System.out.println(a);
+        System.out.println(b);
+
+        Integer aa = 10;
+        Integer bb = aa;
+
+        aa = 4;
+
+        System.out.println(aa);
+        System.out.println(bb);
+
+        Map map = new HashMap();
+        Map map2 = new HashMap();
+
+        map.put("test", "1" );
+        map.put("test2", "1" );
+        map.put("test3", "1" );
+        map.put("test4", "1" );
+
+        map.forEach((k, v) -> {
+            map2.put(k, v);
+            System.out.println(k);
+        });
+
+
+
+
     }
 }
